f305b351f1a7daded14b1f1613d6e62ba0d77728
Tom Banish <tombanish@gmail.com>
2019-11-23 01:00:55 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.scan(/\d{10}/) || phone.scan(/\A\W\d{3}\W\d{3}\W\d{4}\z/) || phone.scan(/\A\d{3}\s\d{3}\s\d{4}\z/)
+  if phone.scan(/\d{10}/) || phone.scan(/\A\W\d{3}\W\d{3}\W\d{4}\z/) || phone.scan(/\A\d{3}\s\d{3}\s\d{4}\z/) || phone.scan(/\A\W\d{3}\W\d{7}\z/)
     return true
   else
     return false
