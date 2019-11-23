7448d9680f043c5c62deef214a6e628935605116
Tom Banish <tombanish@gmail.com>
2019-11-23 00:49:25 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,12 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.scan(/[0-9]{7}/)
-    return true
-  else
-    return false
-  end
-end
+  if phone.scan(//)
 
 
 
